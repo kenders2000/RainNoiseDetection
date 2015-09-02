e22bfe05ac94599d4a0638a5d81a761b36d7adbe
kenders2000 <kenders2000@gmail.com>
2015-09-02 11:13:59 +0100

    Added final algorthm, in trainedalgo (b) and added a test file to test the algorithm

--- featuresFinal.m
+++ featuresFinal.m
@@ -29,52 +29,28 @@ HistimpZ=hist(ImphfZ,L)/length(ImphfZ);
 [null I]=min(abs(cumsum(HistimpZ)-0.1));genAcIn.LhpF90=L(I);
 [null I]=min(abs(cumsum(HistimpZ)-0.5));genAcIn.LhpF50=L(I);
 [null I]=min(abs(cumsum(HistimpZ)-0.9));genAcIn.LhpF10=L(I);
-
-LevelsZ=10*log10(y.^2);
-L=-20:0.05:120;
-HistZ=hist(LevelsZ,L)/length(LevelsZ);
-[null I]=min(abs(cumsum(HistZ)-0.1));genAcIn.L90=L(I);
-[null I]=min(abs(cumsum(HistZ)-0.9));genAcIn.L10=L(I);
-
 L=-60:0.5:60;
 
 HistimpZ=hist(ImphfZ,L)/length(ImphfZ);
 % plot(L,HistimpZ)
 %%  Wavelet packet decomp
  [WPD_E featL feat3 L10 L50 L90 ]  = getmswpfeat(y,round(0.035*Fs),round(0.035*Fs/2),9,'wmtsa');
-% % % % % % % % % % % %    'Hist109'
-% % % % % % % % % % % %     'WPD 288'
-% % % % % % % % % % % %     'WPD 779'
-% % % % % % % % % % % %     'WPD 993'
-% % % % % % % % % % % %     'M'
-% % % % % % % % % % % %     'Hist 83'
-% % % % % % % % % % % %     'WPD 650'
-% % % % % % % % % % % %     'Hist127'
-% % % % % % % % % % % %     'WPD 390'
-% % % % % % % % % % % %     'WPD 282'
-% % % % % % % % % % % %     'Hist 23'
-% % % % % % % % % % % %     'WPD 823'
-% % % % % % % % % % % %     'WPD 352'
-% % % % % % % % % % % %     'Hist217'
-% % % % % % % % % % % %     'moments9'
-% % % % % % % % % % % %     'Hist235'
-% % % % % % % % % % % %     'Hist128'
-
-
-%   'Hist115'
-%     'Hist 88'
-%     'WPD 707'
-%     'WPD 515'
-%     'L90'
-%     'WPD 274'
-%     'WPD 289'
-%     'Hist130'
-%     'Hist 85'
-%     'Hist129'
-%     'WPD 576'
-%     'Hist218'
-%     'Hist  2'
-%     'Hist232'
+%     'Hist115'
+%     'WPD 288'
+%     'WPD 779'
+%     'WPD 993'
+%     'M'
+%     'Hist 89'
+%     'WPD 650'
+%     'Hist133'
+%     'WPD 390'
+%     'WPD 282'
+%     'Hist 29'
+%     'WPD 823'
+%     'WPD 352'
+%     'Hist223'
+%     'moments9'
+%     'Hist241'
 %     'Hist134'
 yf=yhp;
 
@@ -85,10 +61,10 @@ for ii=1:10
 end
 
 %%
-features=[HistimpZ(115) HistimpZ(88) WPD_E(707) WPD_E(515) genAcIn.L90 ...
-    WPD_E(274) WPD_E(289) HistimpZ(130) HistimpZ(85) HistimpZ(129) WPD_E(576)...
-    HistimpZ(218) HistimpZ(2) HistimpZ(232) HistimpZ(134)];
-
+features=[HistimpZ(115) WPD_E(288) WPD_E(779) WPD_E(993) M...
+    HistimpZ(89) WPD_E(650) HistimpZ(133) WPD_E(390) WPD_E(282) ...
+    HistimpZ(29) WPD_E(823) WPD_E(352) HistimpZ(223) oFeats.moment(10)...
+    HistimpZ(241) HistimpZ(134)];
 
 
 
@@ -96,5 +72,5 @@ features=[HistimpZ(115) HistimpZ(88) WPD_E(707) WPD_E(515) genAcIn.L90 ...
 % features=[HistimpZ(117) WPD_E(912) WPD_E(795) HistimpZ(54)  LZmax_imp ...
 %     WPD_E(648)   WPD_E(798) HistimpZ(63) WPD_E(49) HistimpZ(192) HistimpZ(51) HistimpZ(165) ...
 %     HistimpZ(47 ) HistimpZ(175) HistimpZ(52)];
-load trainedalgo4 b;
+load trainedalgo3 b;
 rainYN=str2num(cell2mat(predict(b,features)));
